a636ff6e20c1cb070fc165f1895c3f4068b9c574
Devarsh14 <Devarsh14@gmail.com>
2016-09-23 15:05:44 +1000

    hiu

--- DevLearnCsharp/Program.cs
+++ DevLearnCsharp/Program.cs
@@ -6,16 +6,12 @@ using System.Threading.Tasks;
 
 namespace DevLearnCsharp
 {
+  
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("my first githb project");
-            Console.WriteLine("this is second succesfull execution");
-            Console.WriteLine("this is awesome to use git");
-            Console.Write("this si done");
-            Console.WriteLine("this  is my first line today");
-            Console.Write("this is great to use git");
+          
         }
     }
 }
