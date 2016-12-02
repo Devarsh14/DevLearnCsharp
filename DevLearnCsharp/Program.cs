846beb6a7ff1c3697518d60fdd0ee728f5cca909
Devarsh14 <Devarsh14@gmail.com>
2016-12-02 14:35:43 +1000

    hour of sleep complited

--- DevLearnCsharp/Program.cs
+++ DevLearnCsharp/Program.cs
@@ -13,6 +13,24 @@ namespace DevLearnCsharp
             Console.WriteLine("my first githb project");
             Console.WriteLine("this is second succesfull execution");
             Console.WriteLine("this is awesome to use git");
+            Console.WriteLine("Add user name ");
+            string name =Console.ReadLine();
+            Console.WriteLine("user name is {0}", name);
+            Console.ReadLine();
+            Console.WriteLine("add no of sleep hours");
+            int hourofSleep = int.Parse(Console.ReadLine());
+            if (hourofSleep>8)
+            {
+                Console.WriteLine("User has {0} hour of sleep", hourofSleep);
+                Console.WriteLine("Good sleep bro");
+           
+            }
+            else
+            {
+                Console.WriteLine("you need more sleep");
+            }
+
+            Console.ReadLine();
         }
     }
 }
