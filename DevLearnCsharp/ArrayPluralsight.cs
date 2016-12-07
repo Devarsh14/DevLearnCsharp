84cd8d5aaaea1987cad784107584c2ce0c0cb606
Devarsh14 <Devarsh14@gmail.com>
2016-12-08 01:01:15 +1000

    Foreach and for loop imlementation for Arraylooping

--- DevLearnCsharp/ArrayPluralsight.cs
+++ DevLearnCsharp/ArrayPluralsight.cs
@@ -19,5 +19,22 @@ namespace DevLearnCsharp
             string tues = daysOfWeek[1];
             Console.WriteLine(tues);
         }
+
+
+        public void arrraylooping()
+        {
+            int[] x5 = new int[5] { 22, 33, 55, 66, 54 };
+
+            foreach (int x in x5)
+            {
+                Console.WriteLine("this is my array{0}", x);
+            }
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("New Era array{0}", x5[i]);
+            }
+        }
     }
 }
